d99f9ecaa4cf565700d9833430e09e8b56d3814f
Pedro Mota <PedroHOMota@users.noreply.github.com>
2024-11-06 17:22:40 +0000

    Adding first classes and refactoring

---
+++ com/tus/Main.java
@@ -0,0 +1,13 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.tus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("tst");
+    }
+}
---
+++ com/tus/classes/Item.java
@@ -0,0 +1,53 @@
+/*------------------------------------------------------------------------------
+ *******************************************************************************
+ * COPYRIGHT Ericsson 2024
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Ericsson Inc. The programs may be used and/or copied only with written
+ * permission from Ericsson Inc. or in accordance with the terms and
+ * conditions stipulated in the agreement/contract under which the
+ * program(s) have been supplied.
+ *******************************************************************************
+ *----------------------------------------------------------------------------*/
+
+package com.tus.classes;
+
+import java.util.Date;
+
+public class Item {
+
+    private String name;
+    private Date publishedDate;
+    private int id;
+    private int totalUnits = 0;
+    private int availableUnits = 0;
+
+    public Item(String name, Date publishedDate,int id, int totalUnits, int availableUnits){
+        this.id = id;
+        this.name = name;
+        this.publishedDate = publishedDate;
+        this.availableUnits = availableUnits;
+        this.totalUnits = totalUnits;
+    }
+
+    public Item(String name, Date publishedDate, int totalUnits, int availableUnits){
+        this.name = name;
+        this.publishedDate = publishedDate;
+        this.availableUnits = availableUnits;
+        this.totalUnits = totalUnits;
+    }
+
+    public void increaseUnitsBy(int amount){
+        totalUnits += amount;
+        availableUnits += amount;
+    }
+
+    //Check db first instead of going by field
+    public boolean borrowUnit(){
+        if(availableUnits > 0){
+            availableUnits--;
+            return true;
+        }
+        return false;
+    }
+}
